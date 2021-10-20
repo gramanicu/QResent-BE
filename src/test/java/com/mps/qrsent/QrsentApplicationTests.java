add22f285f6ccd87aed18db080fab115a78f6e09
Grama Nicolae <gramanicu@gmail.com>
2021-10-20 14:00:35 +0300

    Initialized project

---
+++ src/test/java/com/mps/qrsent/QrsentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mps.qrsent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrsentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
